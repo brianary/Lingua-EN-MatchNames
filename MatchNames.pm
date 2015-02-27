c5df704d3889f1e9675638a688b1eed437eb33ee
Brian Lalonde <brian@webcoder.info>
2015-02-27 13:18:43 -0800

    Upgraded ExtUtils::MakeMaker and repackaged (no functionality changes)

--- MatchNames.pm
+++ MatchNames.pm
@@ -124,7 +124,7 @@ use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 use vars qw($debug);
 
-$VERSION=     '1.35';
+$VERSION=     '1.36';
 @ISA=         qw(Exporter);
 @EXPORT=      qw(name_eq);
 @EXPORT_OK=   qw(fname_eq lname_eq);
